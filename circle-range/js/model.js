99294ffaa3fe4a4628eca4a7e101ccf63470e7ea
heartsuit <nxq0108@126.com>
2018-05-16 13:00:22 +0800

    circle-range

---
+++ circle-range/js/model.js
@@ -0,0 +1,12 @@
+var model = [    
+    new BMap.Point(112.558652, 37.812362),
+    new BMap.Point(112.552237, 37.813324),
+    new BMap.Point(112.537164, 37.814194),
+    new BMap.Point(112.558699, 37.80785),
+    new BMap.Point(112.558046, 37.802866),
+    new BMap.Point(112.5585090, 37.8210336),
+    new BMap.Point(112.5584730, 37.8128900),
+    new BMap.Point(112.570709, 37.816631),
+    new BMap.Point(112.550021, 37.820438),
+    new BMap.Point(112.5685200, 37.8232200)
+];
